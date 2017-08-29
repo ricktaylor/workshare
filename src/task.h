e0e0b603019056d3c44009204e4e24c4456ec8e0
Rick Taylor <rick@tropicalstormsoftware.com>
2017-08-29 13:41:06 +0100

    Task param does not need to be const

--- src/task.h
+++ src/task.h
@@ -3,7 +3,7 @@
 #define SRC_TASK_H_
 
 typedef void* task_t;
-typedef void (*task_fn_t)(task_t task, const void* param);
+typedef void (*task_fn_t)(task_t task, void* param);
 
 #define TASK_PARAM_MAX (32 + 64)
 
@@ -19,7 +19,9 @@ scheduler_t scheduler_create(unsigned int threads);
 void scheduler_destroy(scheduler_t sc);
 
 typedef void (*task_parallel_for_fn_t)(void* elems, size_t elem_count, void* param);
-
 task_t task_parallel_for(void* elems, size_t elem_count, size_t elem_size, task_t pt, task_parallel_for_fn_t fn, void* param);
+
+typedef int (*task_parallel_compare_fn_t)(const void*,const void*,void*);
+task_t task_parallel_sort(void* elems, size_t elem_count, size_t elem_size, task_t pt, task_parallel_compare_fn_t fn, void* param);
 
 #endif /* SRC_TASK_H_ */
